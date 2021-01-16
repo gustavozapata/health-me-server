9ed2e4e47d223cc0f2ac8331787aaf16f9ea3d8c
Gustavo Zapata <tavordie@hotmail.com>
2021-01-16 16:17:14 +0000

    remove guide references (route, model, etc)

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const userRouter = require("./routes/userRoutes");
 const bookingRouter = require("./routes/bookingRoutes");
 const stationRouter = require("./routes/stationRoutes");
 const resultRouter = require("./routes/resultRoutes");
-const guideRouter = require("./routes/guideRoutes");
 
 const app = express();
 
@@ -18,6 +17,5 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/bookings", bookingRouter);
 app.use("/api/v1/stations", stationRouter);
 app.use("/api/v1/results", resultRouter);
-app.use("/api/v1/guides", guideRouter);
 
 module.exports = app;
--- routes/guideRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const guideController = require("../controllers/guideController");
-
-router.get("/", guideController.getGuides);
-
-module.exports = router;
