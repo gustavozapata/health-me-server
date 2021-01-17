1cd274cc1b793b518e90681abb868ad598bfbc9f
Gustavo Zapata <tavordie@hotmail.com>
2021-01-17 12:49:09 +0000

    clean up

--- controllers/stationController.js
+++ controllers/stationController.js
@@ -2,7 +2,6 @@ const Station = require("../models/stationModel");
 
 exports.getStations = async (req, res, next) => {
   const stations = await Station.find();
-  console.log(stations)
 
   res.status(200).json({
     status: "success",
